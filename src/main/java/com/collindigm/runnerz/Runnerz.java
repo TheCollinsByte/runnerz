542c8c4bbbcc102a73b914219c0704d797dfe52b
colspan <collin@collindigm.com>
2024-04-03 16:12:06 +0300

    Initiale Commit

---
+++ src/main/java/com/collindigm/runnerz/Runnerz.java
@@ -0,0 +1,11 @@
+package com.collindigm.runnerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Runnerz {
+	public static void main(String[] args) {
+		SpringApplication.run(Runnerz.class, args);
+	}
+}
---
+++ src/test/java/com/collindigm/runnerz/RunnerzTests.java
@@ -0,0 +1,13 @@
+package com.collindigm.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
