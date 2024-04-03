fe6afb2d1c00a265cdbb7826b506280a493e9886
colspan <collin@collindigm.com>
2024-04-03 17:21:37 +0300

    Request Mapping to get all runs with endpoint

--- src/main/java/com/collindigm/runnerz/run/RunController.java
+++ src/main/java/com/collindigm/runnerz/run/RunController.java
@@ -1,15 +1,23 @@
 package com.collindigm.runnerz.run;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class RunController {
+    private final RunRepository runRepository;
 
-    @RequestMapping(path = "/hello", method = RequestMethod.GET)
-    String home() {
-        return "Hello, Welcome to Runnerz";
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    @RequestMapping(path = "/api/runs", method = RequestMethod.GET)
+    List<Run> home() {
+        return runRepository.findAll();
     }
 
 }
