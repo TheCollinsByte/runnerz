6f6fbc1541cc27a80e264f714d0b900367e95485
colspan <collin@collindigm.com>
2024-04-03 16:39:25 +0300

    Build script clean-up and log when runnerz app start successfuly

--- src/main/java/com/collindigm/runnerz/Runnerz.java
+++ src/main/java/com/collindigm/runnerz/Runnerz.java
@@ -1,11 +1,16 @@
 package com.collindigm.runnerz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Runnerz {
+	private static final Logger log = LoggerFactory.getLogger(Runnerz.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Runnerz.class, args);
+		log.info("Runnerz Server Started Successfully");
 	}
 }
