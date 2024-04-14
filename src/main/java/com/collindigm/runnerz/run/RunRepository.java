112165079d2f685be0adeacaddaf80758f7f2dc2
colspan <collin@collindigm.com>
2024-04-14 11:05:24 +0300

    Repository Layer: find run based on location

--- src/main/java/com/collindigm/runnerz/run/RunRepository.java
+++ src/main/java/com/collindigm/runnerz/run/RunRepository.java
@@ -2,5 +2,10 @@ package com.collindigm.runnerz.run;
 
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface RunRepository extends ListCrudRepository<Run, Integer> {
+
+    List<Run> findAllByLocation(String location);
+
 }
