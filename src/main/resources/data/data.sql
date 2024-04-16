5638acdae96fb6afea69bec27b1f5aa5a02bdfeb
colspan <collin@collindigm.com>
2024-04-16 09:56:42 +0300

    uncomment data sql for data insertion

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -1,2 +1,2 @@
---INSERT INTO Run (id, title, started_on, completed_on, miles, location)
---VALUES (1, 'Morning Run', '2024-04-03T17:59:57.708671434', '2024-04-03T19:59:57.708674665', 5, 'OUTDOOR');
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (1, 'Morning Run', '2024-04-03T17:59:57.708671434', '2024-04-03T19:59:57.708674665', 5, 'OUTDOOR');
