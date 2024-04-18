d36a7b3ceea9c8d169f01c201a6ea5ad5dccf1dc
colspan <collin@collindigm.com>
2024-04-18 16:50:54 +0300

    User Rest Client Test Initialized

--- src/test/java/com/collindigm/runnerz/user/UserRestClientTest.java
+++ src/test/java/com/collindigm/runnerz/user/UserRestClientTest.java
@@ -1,7 +1,10 @@
 package com.collindigm.runnerz.user;
 
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@RestClientTest
 class UserRestClientTest {
 
 }
