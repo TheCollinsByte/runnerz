9230d5f63d1af812281c5b7cdbcf3de18477ad17
colspan <collin@collindigm.com>
2024-04-18 13:33:48 +0300

    MVC Test: Should Find All Runs

--- src/test/java/com/collindigm/runnerz/run/RunControllerTest.java
+++ src/test/java/com/collindigm/runnerz/run/RunControllerTest.java
@@ -1,13 +1,57 @@
 package com.collindigm.runnerz.run;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.hamcrest.Matchers.is;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Slice Test: Web Test
  */
 @WebMvcTest(RunController.class)
 class RunControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    RunRepository repository;
+
+    private final List<Run> runs = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        runs.add(new Run(1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(30),
+                3,
+                Location.INDOOR,
+                null));
+    }
+
+    @Test
+    void shouldFindAllRuns() throws Exception {
+        when(repository.findAll()).thenReturn(runs);
+        mvc.perform(MockMvcRequestBuilders.get("/api/runs"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(runs.size())));
+    }
 
 }
