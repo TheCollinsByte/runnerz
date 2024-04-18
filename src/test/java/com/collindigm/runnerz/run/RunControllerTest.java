0aca16298e3598813da4d8cd90f9b20b7ab7f27e
colspan <collin@collindigm.com>
2024-04-18 13:56:13 +0300

    MVC Test: Find run by ID and finding run with a wrong id

--- src/test/java/com/collindigm/runnerz/run/RunControllerTest.java
+++ src/test/java/com/collindigm/runnerz/run/RunControllerTest.java
@@ -3,6 +3,8 @@ package com.collindigm.runnerz.run;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,8 +15,10 @@ import static org.hamcrest.Matchers.is;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -49,19 +53,27 @@ class RunControllerTest {
     @Test
     void shouldFindAllRuns() throws Exception {
         when(repository.findAll()).thenReturn(runs);
-        mvc.perform(MockMvcRequestBuilders.get("/api/runs"))
+        mvc.perform(get("/api/runs"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()", is(runs.size())));
     }
 
     @Test
-    void shouldFindOneRun() {
-
+    void shouldFindOneRun() throws Exception {
+        Run run = runs.get(0);
+        when(repository.findById(ArgumentMatchers.anyInt())).thenReturn(Optional.of(run));
+        mvc.perform(get("/api/runs/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(run.id())))
+                .andExpect(jsonPath("$.title", is(run.title())))
+                .andExpect(jsonPath("$.miles", is(run.miles())))
+                .andExpect(jsonPath("$.location", is(run.location().toString())));
     }
 
     @Test
-    void shouldReturnNotFoundWithInvalidId() {
-
+    void shouldReturnNotFoundWithInvalidId() throws Exception{
+        mvc.perform(get("/api/runs/99"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
