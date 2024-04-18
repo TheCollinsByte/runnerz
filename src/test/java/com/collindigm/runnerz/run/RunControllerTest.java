c9c5e3ce3e8c49a67473352933cf6aad91d57277
colspan <collin@collindigm.com>
2024-04-18 12:29:18 +0300

    Web Controller Layer Test initialized

---
+++ src/test/java/com/collindigm/runnerz/run/RunControllerTest.java
@@ -0,0 +1,13 @@
+package com.collindigm.runnerz.run;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Slice Test: Web Test
+ */
+@WebMvcTest(RunController.class)
+class RunControllerTest {
+
+}
