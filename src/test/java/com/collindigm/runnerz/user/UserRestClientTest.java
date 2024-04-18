006ae5fc91d6495900c85278cca8c293e692e144
colspan <collin@collindigm.com>
2024-04-18 17:15:42 +0300

    Test: find all users

--- src/test/java/com/collindigm/runnerz/user/UserRestClientTest.java
+++ src/test/java/com/collindigm/runnerz/user/UserRestClientTest.java
@@ -1,10 +1,54 @@
 package com.collindigm.runnerz.user;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
-@RestClientTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+
+@RestClientTest(UserRestClient.class)
 class UserRestClientTest {
+
+    @Autowired
+    MockRestServiceServer server;
+
+    @Autowired
+    UserRestClient client;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void shouldFindAllUsers() throws JsonProcessingException {
+        // given
+        User user1 = new User(1,
+                "Leanne",
+                "lgraham",
+                "lgraham@gmail.com",
+                new Address("Kulas Light", "Apt. 556", "Gwenborough", "92998-3874", new Geo(-37.3159, 81.1496)),
+                "1-770-736-8031 x56442",
+                "hildegard.org",
+                new Company("Romaguera-Crona", "Multi-layered client-server neural-net", "harness real-time e-markets"));
+
+        List<User> users = List.of(user1);
+
+        // when
+        this.server.expect(requestTo("https://jsonplaceholder.typicode.com/users"))
+                .andRespond(withSuccess(objectMapper.writeValueAsString(users), MediaType.APPLICATION_JSON));
+
+        // then
+        List<User> allUsers = client.findAll();
+        assertEquals(users, allUsers);
+    }
+
 
 }
