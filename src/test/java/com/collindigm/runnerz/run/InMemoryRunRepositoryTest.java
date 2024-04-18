e90f167f292d750d0253ac5ecee4f803cccf4abb
colspan <collin@collindigm.com>
2024-04-18 11:07:53 +0300

    Tests initialized

--- src/test/java/com/collindigm/runnerz/run/InMemoryRunRepositoryTest.java
+++ src/test/java/com/collindigm/runnerz/run/InMemoryRunRepositoryTest.java
@@ -1,7 +1,62 @@
 package com.collindigm.runnerz.run;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryRunRepositoryTest {
 
+    InMemoryRunRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = new InMemoryRunRepository();
+        repository.create(new Run(
+                1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(30),
+                3,
+                Location.INDOOR,
+                null));
+
+        repository.create(new Run(
+                2,
+                "Wednesday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(60),
+                6,
+                Location.INDOOR,
+                null));
+    }
+
+    @Test
+    void shouldFindAllRuns() {
+        List<Run> runs = repository.findAll();
+        assertEquals(2, runs.size(), "Should have returned 2 runs");
+    }
+
+    @Test
+    void shouldFindRunWithValidId() {
+
+    }
+
+    @Test
+    void shouldNotFindRunWithInvalidId() {
+
+    }
+
+    @Test
+    void shouldCreateNewRun() {
+
+    }
+
+    @Test
+    void shouldUpdateRun() {
+
+    }
 }
